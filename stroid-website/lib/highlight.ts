a2583737d1ecc79b2dbab6edb7aef0b9423853c0
Himesh Bhattarai <himesh.hcb@gmail.com>
2026-04-02 23:43:39 +0545

    status(203): Ducumentation Update

--- stroid-website/lib/highlight.ts
+++
@@ -1,7 +0,0 @@
-﻿import { codeToHtml } from "shiki"
-
-const theme = "github-dark"
-
-export async function highlight(code: string, lang: string = "typescript") {
-  return codeToHtml(code, { lang, theme })
-}
